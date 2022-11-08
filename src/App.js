1ba87902a6f0924f4f1b06ad85645ab56aee045b
mohanraj <mohanwahn0310@gmail.com>
2022-11-08 08:46:47 +0530

    unneccesary files are deleted

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div>
+      <p>fjeijifjei</p>
+      <h1>foujfjei</h1>
     </div>
   );
 }
 
 export default App;
+
